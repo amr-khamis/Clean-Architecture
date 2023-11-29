1d5c778027a76bbfb43eaa54633022675e5a5973
Amr-Khamis <amr92khamis@gmail.com>
2023-11-29 22:42:50 +0200

    Update .gitignore

--- CleanMovie.API/Startup.cs
+++ CleanMovie.API/Startup.cs
@@ -29,6 +29,7 @@ namespace CleanMovie.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // add controller
             services.AddControllers();
             services.AddScoped<IMovieService, MovieService>();
             services.AddScoped<IMovieRepository, MovieRepository>();
